4f283578131fe07ce59c9c6a51546c574c31469f
于博 <2270517836@qq.com>
2021-01-27 00:16:58 +0800

    接口大体完成

---
+++ src/main/java/com/lrm/Exception/NotFoundException.java
@@ -0,0 +1,39 @@
+package com.lrm.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//定义的找不到XX的异常类
+@Controller
+//状态码
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException
+{
+    protected Integer errorCode;
+    protected String errorMsg;
+
+    public NotFoundException(){
+
+    }
+    public NotFoundException(Integer errorCode, String errorMsg) {
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+}
