cce3c27e6769d24e93d04d51eff67e22eb8e75ce
于博 <2270517836@qq.com>
2021-03-25 20:50:14 +0800

    增加发布问题的位置

--- src/main/java/com/lrm/web/IndexController.java
+++ src/main/java/com/lrm/web/IndexController.java
@@ -2,10 +2,13 @@ package com.lrm.web;
 
 import com.lrm.po.Likes;
 import com.lrm.po.Question;
+import com.lrm.po.Tag;
 import com.lrm.po.User;
 import com.lrm.service.LikesService;
 import com.lrm.service.QuestionService;
+import com.lrm.service.TagService;
 import com.lrm.service.UserService;
+import com.lrm.util.FileControl;
 import com.lrm.util.Methods;
 import com.lrm.vo.Magic;
 import com.lrm.vo.Result;
@@ -15,15 +18,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * @author 山水夜止.
@@ -39,6 +46,9 @@ public class IndexController
 
     @Autowired
     private LikesService likesService;
+
+    @Autowired
+    private TagService tagService;
 
     /**
      * 怎么显示有没有点过赞呢？现在不太明白...只能用计算力代替了.
@@ -145,5 +155,116 @@ public class IndexController
         {
             question.setHidden(true);
         }
+    }
+
+    /* 首页发布问题 */
+
+    /**
+     * 有初始化的作用 所有属性都是null.
+     * @return questions:新question对象 tags:第一级标签.
+     */
+    @GetMapping("/questions/input")
+    public Result<Map<String, Object>> input() {
+        Map<String, Object> hashMap = new HashMap<>(2);
+        Question question = new Question();
+        List<Tag> tags = tagService.listTagTop();
+        hashMap.put("questions", question);
+        hashMap.put("tags", tags);
+        return new Result<>(hashMap, true, "");
+    }
+
+    /**
+     * 新增问题 初始化各部分属性.
+     * @param question 前端封装的question对象
+     * @param bindingResult 配合@Valid检测是否为空.
+     * @return 报错信息/成功信息.
+     */
+    @PostMapping("/questions")
+    public Result<Map<String, Object>> post(@Valid Question question, BindingResult bindingResult, HttpServletRequest request)
+    {
+        Map<String, Object> hashMap= new HashMap<>(1);
+        Long userId = Methods.getCustomUserId(request);
+        //后端检验valid 如果校验失败 返回input页面
+        if(bindingResult.hasErrors())
+        {
+            hashMap.put("questions", question);
+            return new Result<>(hashMap, false, "标题、内容、概述均不能为空");
+        }
+        User user = userService.getUser(userId);
+        question.setUser(user);
+        //令前端只传回tagIds而不是tag对象 将它转换为List<Tag> 在service层找到对应的Tag保存到数据库
+        question.setTags(tagService.listTag(question.getTagIds()));
+        Question q;
+
+        if(question.getId() == null)
+        {
+            q = questionService.saveQuestion(question, user);
+        }else
+        {
+            hashMap.put("questions", question);
+            return new Result<>(hashMap, false, "该问题已存在");
+        }
+
+        if (q == null)
+        {
+            return new Result<>(null, false, "发布失败");
+        } else {
+            hashMap.put("questions", question);
+            return new Result<>(hashMap, true,"发布成功");
+        }
+    }
+
+    /**
+     * @param parentTagId 上一级标签Id.
+     * @return 下一级标签集合.
+     */
+    @GetMapping("/questions/{parentTagId}/nextTag")
+    public Result<Map<String, Object>> showNext(@PathVariable Long parentTagId)
+    {
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("tags", tagService.getTag(parentTagId).getSonTags());
+        return new Result<>(hashMap, true, "");
+    }
+
+    /**
+     * @param files 多文件上传
+     * @param questionId 发布问题的Id.
+     * @return 多文件在本地的路径.
+     * @throws IOException 文件大小溢出.
+     */
+    @PostMapping("/questions/uploadPhotos")
+    public Result<Map<String, Object>> uploadPhotos(MultipartFile[] files, HttpServletRequest req, @RequestParam Long questionId) throws IOException {
+        Map<String, Object> hashMap= new HashMap<>(files.length);
+        //创建存放文件的文件夹的流程
+        Long userId = Methods.getCustomUserId(req);
+        SimpleDateFormat sdf = new SimpleDateFormat("/yyyy-MM-dd/");
+        String format = sdf.format(new Date());
+        String path = "/upload/" + userId + "/questions/" + questionId + format;
+        //新文件夹目录绝对路径
+        String realPath = req.getServletContext().getRealPath(path);
+        File folder = new File(req.getServletContext().getRealPath("/upload/" + userId + "/questions/" + questionId));
+        //如果文件夹不存在，创建文件夹 否则删除文件夹
+        if (folder.exists())
+        {
+            FileControl.deleteFile(folder);
+        }
+        List<String> pathList = new ArrayList<>();
+        for (MultipartFile uploadFile : files)
+        {
+            folder = new File(realPath);
+            if (!folder.isDirectory()){
+                folder.mkdirs();
+            }
+            //保存文件到文件夹中
+            //所上传的文件原名
+            String oldName = uploadFile.getOriginalFilename();
+            //新文件名
+            String newName = UUID.randomUUID().toString()+oldName.substring(oldName.lastIndexOf("."));
+            uploadFile.transferTo(new File(folder, newName));
+            pathList.add(realPath + newName);
+        }
+
+        hashMap.put("photos", pathList);
+        return new Result<>(hashMap, true, "上传成功");
     }
 }
