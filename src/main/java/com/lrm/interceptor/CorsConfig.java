bd62b12a56f80e6c5dc1cfa9b879a79013d0a09b
于博 <2270517836@qq.com>
2021-03-25 10:42:40 +0800

    解决跨域问题

--- src/main/java/com/lrm/interceptor/CorsConfig.java
+++ src/main/java/com/lrm/interceptor/CorsConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * @author 来源网络
+ */
 @Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
 
